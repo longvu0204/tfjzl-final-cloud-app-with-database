836d36a2b24f169f18e5a6ecc8e369636d0f6469
ten-tai-khoan-github-cua-ban <email-dang-ky-github-cua-ban@gmail.com>
2026-05-30 07:19:46 -0400

    add content to exam result template

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,34 +1,27 @@
 from django.contrib import admin
-from .models import Course, Lesson, Classes, Question, Choice, Submission 
+from .models import Course, Lesson, Question, Choice, Submission
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
-class ChoiceInline(admin.TabularInline):
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+class QuestionInline(admin.StackedInline):
     model = Choice
     extra = 4
 
-class QuestionInline(admin.TabularInline):
-    model = Question
-    extra = 2
-
-class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline, QuestionInline]
-    list_display = ('name', 'pub_date')
-
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title', 'order']
-
 class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInline]
-    list_display = ['question_text', 'course', 'grade']
+    inlines = [QuestionInline]
+    list_display = ['question_text']
 
 
-
-
-admin.site.register(Course)
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
