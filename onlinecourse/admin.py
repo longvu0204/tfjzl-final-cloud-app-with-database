b9958bac5978c717214a5ac91c4355c86c3b4d53
ten-tai-khoan-github-cua-ban <email-dang-ky-github-cua-ban@gmail.com>
2026-05-30 06:40:24 -0400

    complete final project assessment feature

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,30 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
-
-# <HINT> Register QuestionInline and ChoiceInline classes here
-
+from .models import Course, Lesson, Question, Choice
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
 
-# Register your models here.
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 2
+
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['name', 'description']
-
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['title', 'order']
 
-
-# <HINT> Register Question and Choice models here
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question_text', 'course', 'grade']
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
-admin.site.register(Instructor)
-admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
